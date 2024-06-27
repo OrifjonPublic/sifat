3cf7b7e4f85ddb836010b7c6f3e2486dce89783d
OrifjonPublic <orifjonqahhorov57@gmail.com>
2024-06-27 11:43:02 +0500

    pk changed to id

--- account/urls.py
+++ account/urls.py
@@ -9,14 +9,14 @@ from .views import (
 
 urlpatterns = [
     path('company/', CompanyView.as_view(), name='company'),
-    path('company/<int:pk>/', CompanyEditView.as_view(), name='company_edit'),
+    path('company/<int:id>/', CompanyEditView.as_view(), name='company_edit'),
     path('role/', RoleView.as_view(), name='role'),
-    path('role/<int:pk>/', RoleEditView.as_view(), name='role_edit'),
+    path('role/<int:id>/', RoleEditView.as_view(), name='role_edit'),
     path('product/', ProductView.as_view(), name='product'),
-    path('product/<int:pk>/', ProductEditView.as_view(), name='product_edit'),
+    path('product/<int:id>/', ProductEditView.as_view(), name='product_edit'),
     path('', UserView.as_view(), name='user'),
-    path('<int:pk>/', UserEditView.as_view(), name='user_edit'),
+    path('<int:id>/', UserEditView.as_view(), name='user_edit'),
     path('department/', DepartmentView.as_view(), name='department'),
-    path('department/<int:pk>/', DepartmentDetailView.as_view(), name='department_detail'),
+    path('department/<int:id>/', DepartmentDetailView.as_view(), name='department_detail'),
 
 ]
